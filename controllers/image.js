03b78f1ad38599b8f2610623eb463e2306fe0a10
Aqib Iqbal Ansari <70594146+aqibansari137@users.noreply.github.com>
2020-09-18 11:58:46 +0530

    Add files via upload

---
+++ controllers/image.js
@@ -0,0 +1,29 @@
+const Clarifai = require('clarifai');
+const app = new Clarifai.App({
+    apiKey: '55d4b9cea8cf4db7a08a8ed9f917293d'
+  });
+
+const handleApiCall=(req,res)=>{
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data=>{
+        res.json(data);
+    })
+    .catch(err=>res.status('400').json('Unable to work with Api'))
+}
+
+const handleImage=(req, res, db) => {
+    const { id } = req.body;
+    db('users')
+        .where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(404).json('Unable to get entries'));
+}
+
+module.exports={
+    handleImage:handleImage,
+    handleApiCall:handleApiCall
+}
